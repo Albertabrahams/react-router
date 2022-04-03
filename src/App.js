b359ce7cf723d40a7415b9e496eeb49fcf12397e
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-04-03 22:48:26 +0300

    1.3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer';
 import Navbar from './components/Navbar';
-import { BrowserRouter } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Products from './pages/Products';
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar/>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
-      
     </div>
   );
 }
 
-export default App;
+export default App
